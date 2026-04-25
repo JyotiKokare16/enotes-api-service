5fa91cc1c9cc3d186d6a5e98a7a598b272454042
JyotiKokare16 <kokarejyoti004@gmail.com>
2026-04-25 13:04:39 +0530

    Added category module

---
+++ src/main/java/com/enotes/entity/BaseModel.java
@@ -0,0 +1,32 @@
+package com.enotes.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseModel {
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @Column(name = "created_on")
+    private Date createdOn;
+
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
+    @Column(name = "updated_on")
+    private Date updatedOn;
+}
