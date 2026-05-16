ab261f411b018ecb42051a33df0f94886308a82b
JyotiKokare16 <kokarejyoti004@gmail.com>
2026-05-16 16:22:15 +0530

    Added DTO and config classes

---
+++ src/main/java/com/enotes/dto/CategoryDto.java
@@ -0,0 +1,36 @@
+package com.enotes.dto;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Column(name = "created_by")
+    private Integer createdBy;
+
+    @Column(name = "created_on")
+    private Date createdOn;
+
+    @Column(name = "updated_by")
+    private Integer updatedBy;
+
+    @Column(name = "updated_on")
+    private Date updatedOn;
+
+}
