db397653fa005eb53638fe33d5436fc6bec4314e
JyotiKokare16 <kokarejyoti004@gmail.com>
2026-05-17 12:45:52 +0530

    get & delete implementation

--- src/main/java/com/enotes/controller/CategoryController.java
+++ src/main/java/com/enotes/controller/CategoryController.java
@@ -5,44 +5,78 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.enotes.entity.Category;
+import com.enotes.dto.CategoryDto;
+import com.enotes.dto.CategoryResponse;
 import com.enotes.service.CategoryService;
+
 
 @RestController
 @RequestMapping("/api/v1/category")
 public class CategoryController {
 
 	@Autowired
-    private  CategoryService categoryService;
+	private CategoryService categoryService;
 
-    // Constructor Injection (Best Practice)
-   
+	@PostMapping("/save")
+	public ResponseEntity<?> saveCategory(@RequestBody CategoryDto categoryDto) {
 
-    @PostMapping("/save-category")
-    public ResponseEntity<String> saveCategory(@RequestBody Category category) {
+		Boolean saveCategory = categoryService.saveCategory(categoryDto);
+		if (saveCategory) {
+			return new ResponseEntity<>("saved success", HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<>("not saved", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-        Boolean saveCategory = categoryService.saveCategory(category);
+	@GetMapping("/")
+	public ResponseEntity<?> getAllCategory() {
 
-        if (saveCategory) {
-            return new ResponseEntity<>("saved", HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity<>("not saved", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+		List<CategoryDto> allCategory = categoryService.getAllCategory();
+		if (CollectionUtils.isEmpty(allCategory)) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return new ResponseEntity<>(allCategory, HttpStatus.OK);
+		}
 
-    @GetMapping("/category")
-    public ResponseEntity<?> getAllCategory() {
+	}
 
-        List<Category> allCategory = categoryService.getAllCategory();
+	@GetMapping("/active")
+	public ResponseEntity<?> getActiveCategory() {
 
-        if (CollectionUtils.isEmpty(allCategory)) {
-            return ResponseEntity.noContent().build();
-        } else {
-            return new ResponseEntity<>(allCategory, HttpStatus.OK);
-        }
-    }
+		List<CategoryResponse> allCategory = categoryService.getActiveCategory();
+		if (CollectionUtils.isEmpty(allCategory)) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return new ResponseEntity<>(allCategory, HttpStatus.OK);
+		}
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getCategortDetailsById(@PathVariable Integer id) {
+		CategoryDto categoryDto = categoryService.getCategoryById(id);
+		if (ObjectUtils.isEmpty(categoryDto)) {
+			return new ResponseEntity<>("Category not found with Id=" + id, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(categoryDto, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteCategoryById(@PathVariable Integer id) {
+		Boolean deleted = categoryService.deleteCategory(id);
+		if (deleted) {
+			return new ResponseEntity<>("Category deleted success", HttpStatus.OK);
+		}
+		return new ResponseEntity<>("Category Not deleted", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
 }
